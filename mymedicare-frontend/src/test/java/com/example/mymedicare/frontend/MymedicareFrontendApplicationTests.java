3a318076335407ff57d1d39085c990732f87a8f0
ARRakshitha <rakshithaar878@gmail.com>
2023-04-02 20:02:21 +0530

    frontend commit

---
+++ mymedicare-frontend/src/test/java/com/example/mymedicare/frontend/MymedicareFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymedicare.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymedicareFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
